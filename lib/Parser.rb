412c5daec764da584ce83907199927f5eeab71fc
hazel <connellsensational@gmail.com>
2023-10-21 13:35:00 -0400

    working on parser and cli

--- lib/Parser.rb
+++ lib/Parser.rb
@@ -6,9 +6,28 @@ module FlyingStateMachine
         # whitespace
         rule(:spaces) { match("\s").repeat(1) }
         rule(:spaces?) { spaces.maybe }
+
+        rule(:newline) { match("\n").repeat(1) }
+        rule(:newline?) { newline.maybe }
         
         # data
         rule(:bit) { str('0') | str('1') }
+
+        # input for the machine
+        rule(:input) {
+            str("input") >> spaces? >> bit.repeat
+        }
+
+        # state machine nodes
+        rule(:node) {
+            (state | machine) >> newline
+        }
+
+        # states
+        rule(:state) {
+            str("state") 
+        }
+
 
         # state machine declaration
         rule(:machine) {
@@ -17,15 +36,13 @@ module FlyingStateMachine
             any >>
             spaces? >>
             str("{") >>
-
+            newline? >>
+            node.repeat >> 
             str("}")
         }
     
         # what the parser looks for first
-        root(:machine)
+        root(:input)
     end
     
 end
-
-parser = FlyingStateMachine::Parser.new
-p parser.parse('machine A {}')
